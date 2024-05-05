d097947b15dea222f95320e5f2a13e118fd376f2
Atmadip <52615332+Atmadip@users.noreply.github.com>
2024-05-05 17:54:58 -0500

    Update gray_to_binary_tb.v

--- gray_to_binary_tb.v
+++ gray_to_binary_tb.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/20/2024 11:58:17 PM
-// Design Name: 
-// Module Name: gray_to_binary_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module gray_to_binary_tb;
 parameter data_width = 4;
